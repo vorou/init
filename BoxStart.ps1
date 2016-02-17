d8c5eda3e3a8c4fa59f2dc40485e032fa6e757cc
vorou <vorou@skbkontur.ru>
2016-02-17 12:41:01 +0500

    full paths

--- BoxStart.ps1
+++ BoxStart.ps1
@@ -1,5 +1,8 @@
+$git = "C:\Program Files\Git\cmd\git.exe"
+$apm = "C:\Users\vorou\AppData\Local\atom\bin\apm.cmd"
+
 cinst -y git
 
 cinst -y atom
-git clone http://github.com/vorou/.atom C:\Users\vorou\.atom
-apm install --packages-file C:\Users\vorou\code\init\packages.txt
+& $git clone http://github.com/vorou/.atom C:\Users\vorou\.atom
+& $apm install --packages-file C:\Users\vorou\code\init\packages.txt
