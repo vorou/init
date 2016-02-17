dc7156f2d9b6a56c9e161a3c1535f990eeb15690
vorou <vorou@skbkontur.ru>
2016-02-17 14:09:04 +0500

    even better paths

--- BoxStart.ps1
+++ BoxStart.ps1
@@ -4,5 +4,6 @@ $apm = join-path $env:localappdata "atom\bin\apm.cmd"
 cinst -y git
 
 cinst -y atom
-& $git clone http://github.com/vorou/.atom C:\Users\vorou\.atom
-& $apm install --packages-file C:\Users\vorou\code\init\packages.txt
+$atomConfig = join-path $env:home ".atom"
+& $git clone http://github.com/vorou/.atom $atomConfig
+& $apm install --packages-file (join-path $atomConfig packages.txt)
